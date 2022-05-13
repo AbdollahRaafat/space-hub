bd71c06b0d1daa642d33c9c5b48276a0a4115b01
joel <joelkalema63@gmail.com>
2022-05-13 16:58:04 +0200

    Added API

--- src/Components/Mission.js
+++ src/Components/Mission.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import '../styles/navbar.css';
 import MissionData from './missionData'
+import { getRocketList } from '../redux/resevations/confugStore';
 
 const Mission = () => {
+  const dispatchAction = useDispatch();
+  const mission = useSelector((state) => state.resrvationReducer);
+  useEffect(() => {
+    dispatchAction(getRocketList());
+  }, [dispatchAction]);
+  
   return (
-    <div>
-      <p>Mission</p>
-      <MissionData />
+    mission.map((r) => (
+      <div key={r.id}>
+      <MissionData
+       name={r.name}
+       desc={r.desc}
+      />
     </div>
+    ))
   );
 };
 
--- src/Components/missionData.js
+++ src/Components/missionData.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import '../styles/navbar.css';
-import { useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
 
-const MissionData = () => {
-  const data = useSelector((state) => state.resrvationReducer);
+const MissionData = ({
+  desc, name,
+}) => {
+
   return (
     <div className='mission'>
-      <h1>{data.name}</h1>
-      <p>{data.description}</p>
-      <button>{data.stateMember}</button>
-      <button>{data.stateJoin}</button>
+      <h1>{name}</h1>
+      <p>{desc}</p>
     </div>
   );
+};
+
+MissionData.propTypes = {
+  name: PropTypes.string.isRequired,
+  desc: PropTypes.string.isRequired,
 };
 
 export default MissionData;
